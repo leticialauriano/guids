a188346001c3f86810d490adb7b89d32f45aadfe
Letícia Lauriano <114084854+leticialauriano@users.noreply.github.com>
2023-08-16 16:36:11 -0300

    Adicionando projeto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Guids
+{
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var id = Guid.NewGuid(); //gerar ele
+            id.ToString();
+
+            id = new Guid("número gerado");
+            if (id == Guid.NewGuid()) //comparação
+
+            Console.WriteLine(id.ToString().Substring(0,8));
+
+        }
+    }
+
+}
